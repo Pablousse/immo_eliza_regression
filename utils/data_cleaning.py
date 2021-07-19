aa4e8a2c998e2d15830c2187cce547ca8be1fe4a
hakan ergin <hakan.ergin@gmail.com>
2021-07-19 09:38:29 +0200

    removed unnecessary lines

--- utils/data_cleaning.py
+++ utils/data_cleaning.py
@@ -6,43 +6,17 @@ import pandas as pd
 import seaborn as sns
 
 
-# %%
-df = pd.read_csv('./assets/houses.csv')
-df
+df = pd.read_csv('../assets/houses.csv')
 
-
-# %%
-df.count()
-
-
-# %%
-df.shape
-
-
-# %%
 # removes about 1500 duplicates
 df = df.drop_duplicates(subset=['location', 'price', 'area', 'building_condition'])
 
-
-# %%
-df.shape
-
-
-# %%
 # removing any row that contains empty area information because it's a key feature
 df = df.dropna(subset=['area'])
 
-
-# %%
-df.count()
-
-
-# %%
 # initial cleaning on the columns that have a lot of missing values
 df = df.drop(columns=['terrace_area', 'garden_area', 'terrace_area'])
 
-
-# %%
 plt.figure(figsize = (16,5))
 
 sns.heatmap(data=df.corr(), annot=True, cmap='BuPu')
