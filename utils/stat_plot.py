782304c636140b5546e6575400f4214c35e1b8b9
Durieux <durieux.arnaud@hotmail.com>
2021-07-19 11:14:40 +0200

    Implemented pairplot and correlation matrix

--- utils/stat_plot.py
+++ utils/stat_plot.py
@@ -1,26 +1,19 @@
 import pandas as pd
-
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 df = pd.read_csv("assets/houses.csv")
-new_df = df[["price", "area", "kitchen_equipped", "room_number", "furnished"]]
 
-df = transform_categorical_feature(df, "type", "is_type_")
+# Showing pairplots
+new_df = df[["price", "area", "room_number"]]
+y_column = ["price"]
+x_column = list(new_df.columns)
+x_column.remove(x_column[0])
+g = sns.pairplot(new_df, x_vars=x_column, y_vars=y_column)
+g.fig.set_size_inches(10, 10)
 
-
-# sns.pairplot(new_df)
-# plt.show()
-
-# X = df
-# y = X.pop("price")
-# discrete_features = ["type", "subtype", "location", "kitchen_equipped", "furnished", "fireplace", "terrace", "garden", "swimming_pool", "building_condition"]
-
-
-
-
-# corr = df.corr()
-
-# top_corr_features = corr.index
-# plt.figure(figsize=(20,20))
-# #plot heat map
-# g=sns.heatmap(corr,annot=True,cmap="coolwarm", square=True, fmt=".2f")
-# plt.show()
+# Plotting correlation matrix
+corr = df.corr()
+plt.figure(figsize=(20, 20))
+sns.heatmap(corr, annot=True, cmap="coolwarm", square=True, fmt=".2f")
+plt.show()
