954a268c2076c2ab50a69a76ed8de443f0cc6415
Durieux <durieux.arnaud@hotmail.com>
2021-07-20 15:21:20 +0200

    cleaned code and added annotation

--- utils/random_forest_model.py
+++ utils/random_forest_model.py
@@ -1,25 +1,44 @@
-from utils.data_cleaners import clean_df, remove_outliers, transform_categorical_feature
+from typing import Optional
+
+import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
-import pandas as pd
+
+from utils.data_cleaners import clean_df, remove_outliers, transform_categorical_feature
 
 
-def create_random_forest_model(dataset_type="") -> RandomForestRegressor:
+def create_random_forest_model(
+    dataset_type: Optional[str] = "",
+) -> RandomForestRegressor:
     df = pd.read_csv("assets/houses.csv")
     df = clean_df(df)
     df = remove_outliers(df)
     if dataset_type != "":
-        df = df.loc[df['type'] == dataset_type]
+        df = df.loc[df["type"] == dataset_type]
 
-    df = df.drop(["id", "type", "kitchen_equipped", "fireplace", "terrace", "garden",
-                  "swimming_pool", ], axis=1)
+    df = df.drop(
+        [
+            "id",
+            "type",
+            "kitchen_equipped",
+            "fireplace",
+            "terrace",
+            "garden",
+            "swimming_pool",
+        ],
+        axis=1,
+    )
     df = transform_categorical_feature(df, "subtype", "is_subtype_")
-    df = transform_categorical_feature(df, "building_condition", "is_building_condition_")
+    df = transform_categorical_feature(
+        df, "building_condition", "is_building_condition_"
+    )
     df = transform_categorical_feature(df, "location", "zipcode_")
     y = df.price.to_numpy().reshape(-1, 1)
     ndf = df.drop(["price"], axis=1)
     x = ndf.to_numpy()
-    X_train, X_test, y_train, y_test = train_test_split(x, y, random_state=42, test_size=0.2)
+    X_train, X_test, y_train, y_test = train_test_split(
+        x, y, random_state=42, test_size=0.2
+    )
     reg = RandomForestRegressor(random_state=0).fit(X_train, y_train)
     print(f"Score = {reg.score(X_test,y_test)}")
 
